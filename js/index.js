1003e47e0e81e216da7e48d0262c19c4a13be7db
mrbuckle <96025459+mrbuckle@users.noreply.github.com>
2022-05-28 02:30:18 -0400

    html css js
    polished new about me css
    new skills html and css plus some old css
    old work html and css
    new media query old media querry
    jquery console log imported css animation from web for test on chevron
    media queery for about also root media query to have things shrink to rem

---
+++ js/index.js
@@ -0,0 +1,5 @@
+console.log("Kevin Rules!")
+
+$("#downloadRes").on("click", function() {
+    window.open("https://docs.google.com/document/d/1L90qUYb8xQDsNoapPMkyhUAqaY5wRfFnWteRnc0OozU/edit?usp=sharing");
+});
